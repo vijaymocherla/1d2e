cd4037994cadd263135940d4915e38a5f70aa724
Sai Vijay Mocherla <vijaysai.mocherla@gmail.com>
2023-11-26 02:30:17 +0530

    fixed compiler options

---
+++ fortran/run_test_2e_dvr.sh
@@ -0,0 +1,18 @@
+compiler='ifort'
+
+intel_flags='-qmkl -qopenmp'
+gnu_flags='-llapack -lopenblas -fopenmp'
+
+if [ $compiler == 'ifort' ]; then
+    flags=$intel_flags
+elif [ $compiler == 'gfortran' ]; then
+    flags=$gnu_flags
+fi
+
+cd tests/
+
+$compiler -c test_2e_dvr.f90 ../src/exact_2e_dvr.f90 ../src/blas_wrappers.f90 ../src/lapack_wrappers.f90 $flags -heap-arrays
+$compiler test_2e_dvr.o lapack_wrappers.o blas_wrappers.o exact_2e_dvr.o -o test_2e_dvr.x $flags -heap-arrays
+# ./test_2e_dvr.x 
+
+
--- fortran/run_tests.sh
+++ fortran/run_tests.sh
@@ -14,10 +14,10 @@ cd tests/
 # $compiler -c test_matmul.f90 -I../bin
 # $compiler -c test_ho_dvr.f90 -I../bin
 
-$compiler  test_matmul.f90 ../src/* -o test_matmul.x $flags
+$compiler  test_matmul.f90 ../src/* -o test_matmul.x $flags 
 ./test_matmul.x
 
-$compiler  test_ho_dvr.f90 ../src/* -o test_ho_dvr.x $flags
-./test_ho_dvr.x
+$compiler  test_ho_dvr.f90 ../src/* -o test_ho_dvr.x $flags -heap-arrays
+./test_ho_dvr.x 
 
 
