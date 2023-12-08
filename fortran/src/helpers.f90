8740029e23dd4643d18f949a8798941dcea5f7bc
Sai Vijay Mocherla <vijaysai.mocherla@gmail.com>
2023-12-08 18:48:53 +0530

    added omp based helpers

--- fortran/src/helpers.f90
+++ fortran/src/helpers.f90
@@ -1,10 +1,12 @@
 module helpers
     use, intrinsic ::iso_fortran_env, only:dp=>real64
+    use omp_lib
     implicit none
     
     private
 
-    public zexp_array, dexp_array
+    public zexp_array, dexp_array, &
+           omp_normalize, omp_daxpy, omp_dotprod
 
     contains
 
@@ -35,6 +37,59 @@ module helpers
             exp_x(i,i) = exp(tmp) 
         end do
     end subroutine
+
+    ! parallelised dot product of two vectors
+    subroutine omp_dotprod(psi_i, psi_j, result)
+        implicit none
+        real(dp), dimension(:), intent(in)  :: psi_i, psi_j
+        real(dp), intent(out) :: result
+        integer :: u
+        integer :: ndim
+        result = 0.0d0
+        ndim = size(psi_i)
+        !$omp parallel shared(ndim, psi_i, psi_j) private(u)
+        !$omp do reduction ( + : result )
+        do u = 1, ndim
+            result = result + psi_i(u) * psi_j(u)
+        end do
+        !$omp end do
+        !$omp end parallel
+    end subroutine
+
+    ! parallelised constant times a vector plus a vector ( with double precision) 
+    subroutine omp_daxpy(a, x_array, y_array, z_array)
+        implicit none
+        real(dp), intent(in) :: a
+        real(dp), dimension(:), intent(in)  :: x_array, y_array
+        real(dp), dimension(:), intent(out) :: z_array
+        integer :: i, ndim
+        ndim = size(x_array)
+        z_array = 0.0d0
+        !$omp parallel shared(ndim, a, x_array, y_array) private(i)
+        !$omp do
+        do i=1,ndim
+            z_array(i) = a*x_array(i) + y_array(i)
+        end do
+        !$omp end do
+        !$omp end parallel
+    end subroutine
+
+    ! parallelised vector normalisation
+    subroutine omp_normalize(psi)
+        real(dp), intent(inout) :: psi(:)
+        real(dp) :: norm, inv_norm
+        integer :: u, ndim
+        ndim = size(psi)
+        call omp_dotprod(psi, psi, norm)
+        inv_norm = 1.0d0/(norm)**(0.5)
+        !$omp parallel shared(ndim, inv_norm, psi) private(u)
+        !$omp do
+        do u=1,ndim
+            psi(u) = inv_norm * psi(u)
+        end do
+        !$omp end do 
+        !$omp end parallel
+    end subroutine omp_normalize
 
 
 end module helpers
